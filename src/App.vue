1eed33f34d3723183d229bba5d7d5c778d368098
Julian2206 <julian.pcc991@gmail.com>
2022-07-14 16:26:20 +0200

    miles-1(added-Title)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="La mia prima app con Vue CLI!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/Title.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss">
+div {
+  color: royalblue;
+  font-size: 25px;
+  font-weight: bold;
+}
+</style>
